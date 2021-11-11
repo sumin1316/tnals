b01227d8b535e6ff7de6e9022bc14ba174e3348d
sumin1316 <94099610+sumin1316@users.noreply.github.com>
2021-11-11 16:10:53 +0900

    01아이콘과 소리 입력

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.happy.playUntilDone()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.House)
+    soundExpression.twinkle.playUntilDone()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    soundExpression.soaring.playUntilDone()
+})
 basic.forever(function () {
 	
 })
