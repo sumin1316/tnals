2d767da5c3e76da4626c152e10ef4ad16508d59b
sumin1316 <94099610+sumin1316@users.noreply.github.com>
2021-11-11 16:44:46 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,5 +1,27 @@
+let light_on = false
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
+    soundExpression.happy.playUntilDone()
+})
+input.onSound(DetectedSound.Loud, function () {
+    light_on = !(light_on)
+    if (light_on) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Scissors)
     soundExpression.happy.playUntilDone()
 })
 input.onButtonPressed(Button.AB, function () {
@@ -10,6 +32,37 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
     soundExpression.soaring.playUntilDone()
 })
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.happy.playUntilDone()
+})
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1000)
+})
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.clearScreen()
+    }
 })
